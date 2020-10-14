382c92ce1d4f8085389045615061bb94101c21b2
PopovDmitriy <447039048dp@gmail.com>
2020-10-14 23:13:21 +0300

    #2 Header + icons

---
+++ src/icons.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SVG from 'react-inlinesvg';
+
+const ArrowIcon = () => <SVG src={require('./icons/arrow-icon.svg')} />;
+const AvatarIcon = () => <SVG src={require('./icons/avatar-icon.svg')} />;
+const HomeIcon = () => <SVG src={require('./icons/home-icon.svg')} />;
+const LikeIcon = () => <SVG src={require('./icons/like-icon.svg')} />;
+const PlusIcon = () => <SVG src={require('./icons/plus-icon.svg')} />;
+const SearchIcon = () => <SVG src={require('./icons/search-icon.svg')} />;
+
+export {
+    ArrowIcon,
+    AvatarIcon,
+    HomeIcon,
+    LikeIcon,
+    PlusIcon,
+    SearchIcon
+};
